e9e821aa8dc9d613bb6009ccc7b56122462fdcb5
Figment <17240592+FigmentBoy@users.noreply.github.com>
2021-01-10 16:10:55 -0600

    Update CCMenuItemSpriteExtra.h

--- template/default-cpp/cappuccino-sdk/incl/cocos2dx/custom/Sprites/CCMenuItemSpriteExtra/CCMenuItemSpriteExtra.h
+++ template/default-cpp/cappuccino-sdk/incl/cocos2dx/custom/Sprites/CCMenuItemSpriteExtra/CCMenuItemSpriteExtra.h
@@ -8,5 +8,5 @@ public:
 	virtual void selected() override;
 	virtual void unselected() override;
 	static CCMenuItemSpriteExtra* create(CCNode *normalSprite, CCNode *selectedSprite, CCObject *target, cocos2d::SEL_MenuHandler selector);
-	void setSizeMult(float multiplier) { m_sizeMult = 1.25; }
+	void setSizeMult(float multiplier) { m_sizeMult = multiplier; }
 };
